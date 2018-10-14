b08afda378b2bb746a9a68fed516bb242b660842
byron <byron.d.wheeler@outlook.com>
2018-10-14 13:22:25 -0500

    started bash track

---
+++ bash/hello-world/hello_world.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This option will make the script exit when there is an error
+set -o errexit
+# This option will make the script exit when it tries to use an unset variable
+set -o nounset
+
+main() {
+  echo "Hello, World!"
+  return 0
+}
+
+# Calls the main function passing all the arguments to it via '$@'
+# The argument is in quotes to prevent whitespace issues
+main "$@"
