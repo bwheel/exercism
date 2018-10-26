4e5e0bfad052b15560ed8ad5d26dda5c4c0b7cc4
Wheeler <wheelerbyron@ad.garmin.com>
2018-10-26 10:03:56 -0500

    update elixir rna-transcription

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,18 +9,15 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-  dna 
-      |> Enum.map( fn nucleotide -> transcribe(nucleotide) end) 
-      |> Enum.join 
-      |> String.to_charlist
+    Enum.map( dna, &transcribe/1 ) 
   end
 
   defp transcribe(nucleotide) do
     case nucleotide do
-      ?G -> 'C'
-      ?C -> 'G'
-      ?T -> 'A'
-      ?A -> 'U'
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end
   end
 end
