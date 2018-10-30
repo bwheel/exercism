11c34dc41e354b61ce5b267ca5fa029c3dbcf4c8
Wheeler <wheelerbyron@ad.garmin.com>
2018-10-30 08:12:32 -0500

    add csharp sum-of-multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return Enumerable
+            .Range(0, max)
+            .SelectMany( i => multiples.Select( mul => i % mul == 0 ? i : 0) ) 
+            .Distinct()
+            .Sum();
+    }
+}
