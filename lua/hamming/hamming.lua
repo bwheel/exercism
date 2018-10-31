879e449f564d446253f432ee9a1faf87a078775c
Byron Wheeler <bwheel1990@gmail.com>
2018-10-31 16:30:19 -0500

    fix lua hamming

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,12 +1,19 @@
 local hamming = {}
 
 function hamming.compute(seq1, seq2)
-    local result = false;
-    for i = 1, seq1.length() do
-        local e1 = string.sub(seq1, i, i)
-        local e2 = string.sub(seq2, i, i)
-        result = result & e1 == e2
+    local result = 0;
+    if seq1:len() ~= seq2:len() then
+        result = -1
+    else
+        for i = 1, #seq1 do
+            local e1 = seq1:sub(i, i)
+            local e2 = seq2:sub(i, i)
+            if e1 ~= e2 then 
+                result = result + 1
+            end
+        end
     end
+    return result
 end
 
 return hamming
