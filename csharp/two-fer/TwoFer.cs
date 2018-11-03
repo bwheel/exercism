88c6c3f14505a8019c8a11b8313489ab2119c8ec
byron <byron.d.wheeler@outlook.com>
2018-11-03 10:18:58 -0500

    submit csharp twofer

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      private const string template = "One for {0}, one for me.";
+      public static string Name(string input = null)
+      {
+          return string.Format(template, input??"you");
+      }
+  }
