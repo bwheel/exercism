9ace1e77a742ac3f23c173ab7398cee193e12386
Byron Wheeler <1660767+bwheel@users.noreply.github.com>
2019-02-08 09:19:56 -0600

    csharp isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        return string.IsNullOrWhiteSpace(word) ||
+                    word
+                    .Replace("-","")  // don't count hyphens
+                    .Replace(" ", "") // don't count spaces
+                    .ToLower()        // case insensitive.
+                    .ToArray()        // convert to collection
+                    .GroupBy( c => c ) // group by individusl letters
+                    .Where( c => c.Count() > 1) // filter out non multiples
+                    .Count() < 1; // if there are no matches, it's an isogram.
+    }
+}
