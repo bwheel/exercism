3ce4c418b941c176aefcf5fad0d977980eb67809
byron <bwheel1990@gmail.com>
2020-04-21 19:15:20 -0500

    Add F# exercises

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,8 @@
+﻿module Leap
+
+let leapYear (year: int): bool = 
+    let isDivisibleBy4 = year % 4 = 0
+    let isDivisibleBy100 = year % 100 = 0
+    let isDivisibleBy400 = year % 400 = 0
+    
+    (isDivisibleBy4 && not isDivisibleBy100) || isDivisibleBy400
