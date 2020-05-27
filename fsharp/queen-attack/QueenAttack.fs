6648252bf61f0782ff0a0469ce0df6bf483b3d4d
byron <bwheel1990@gmail.com>
2020-05-26 22:17:36 -0500

    Fsharp QueenAttack decompose parameters

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,29 +1,20 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = 
-    let inBounds(d:int) =
+let create (x: int32, y: int32) = 
+    let inBounds(d:int32) =
         d > -1 && d < 8
-    position |> fst |> inBounds 
-    && position |> snd |> inBounds
+    inBounds x && inBounds y
 
 let canAttack (queen1: int * int) (queen2: int * int) = 
-    let canDiagonalAttack(queen1: int * int, queen2:int * int) =
-        let x1, y1 = queen1
-        let x2, y2 = queen2
+    let canDiagonalAttack((x1: int32, y1: int32), (x2: int32, y2: int32)) =
         let xdiff = x1 - x2
         let ydiff = y1 - y2
         ydiff <> 0 && xdiff <> 0 // Check for divide-by-zero
         && abs(xdiff/ydiff) = 1  // if the slope is 1, it's a pure diagonal
 
-    let canRowAttack(queen1: int * int, queen2: int * int) = 
-        let x1, _ = queen1
-        let x2, _ = queen2
-        x1 = x2
+    let canRowAttack((x1:int32, _), (x2: int32, _)) = x1 = x2
     
-    let canColumnAttack(queen1: int * int, queen2: int * int) = 
-        let _, y1 = queen1
-        let _, y2 = queen2
-        y1 = y2
+    let canColumnAttack((_, y1:int32), (_, y2: int32)) = y1 = y2
 
     canDiagonalAttack(queen1, queen2) 
     || canRowAttack(queen1, queen2) 
